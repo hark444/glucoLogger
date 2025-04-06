730e81659fcd9e0fda79612af97babbd815789d7
Harshad Keshwani <hkeshwani68@gmail.com>
2025-04-07 04:36:56 +0530

    Added user profile section

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('home/', views.account_home, name='home'),
-    path('login/', views.user_login, name='login')
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('profile_update/', views.update_user, name='profile_update')
 ]
--- users/views.py
+++ users/views.py
@@ -1,14 +1,20 @@
 import logging
 from utils.utils import POST_STR
+from .forms import UserProfileUpdateForm
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from .template_registries import USER_REGISTRATION_TEMPLATE, USER_BASE_TEMPLATE
+from .template_registries import (
+    USER_REGISTRATION_TEMPLATE, USER_BASE_TEMPLATE, USER_LOGIN_TEMPLATE, USER_PROFILE_UPDATE_TEMPLATE
+)
 
 
 logger = logging.getLogger(__name__)
 
 
+@login_required(login_url="/accounts/login")
 def account_home(request):
     return render(request, USER_BASE_TEMPLATE, {})
 
@@ -58,4 +64,23 @@ def user_login(request):
     else:
         form = AuthenticationForm()
 
-    return render(request, 'users/login.html', context={'form': form})
+    return render(request, USER_LOGIN_TEMPLATE, context={'form': form})
+
+
+def user_logout(request):
+    logger.info(f"Logging out the current user {request.user}")
+    logout(request)
+    return redirect("home")
+
+
+@login_required
+def update_user(request):
+    if request.method == POST_STR:
+        form = UserProfileUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            logger.info(f"Updating user: {request.user}")
+            form.save()
+
+    form = UserProfileUpdateForm(instance=request.user)
+
+    return render(request, USER_PROFILE_UPDATE_TEMPLATE, context={'form': form})
